99c0de91fad469c3fe4aab950cad5920ac26dac5
matte <mmatteoderosa@gmail.com>
2026-01-29 23:26:58 +0100

    fix mouse over

--- javascript/lezioni/MouseOver/script.js
+++ javascript/lezioni/MouseOver/script.js
@@ -1,27 +1,16 @@
-let original = [];
-
-function change(which){
-
-    const imgList = ['Acqua', 'Arance', 'Biscotti'];
-
-    const position = "./foto/";
-    const img = document.images[which];
-
-    if(!original[which])
-    {
-        original[which] = img.src;
-        img.src = position + imgList[which] + "Selected.jpg";
-    }
-    else
-    {
-        img.src = original[which];
-        original[which] = null;
-    }
-}
-
-function write(text){
+function changeText(n){
     const p = document.getElementById("paragraph");
 
-    p.innerHTML = text;
+    const a = "mouse sopra l'immagine";
+    const b = "testo default del paragrafo";
+
+    switch (n){
+        case 0:
+            p.innerHTML = a;
+            break;
+        case 1:
+            p.innerHTML = b;
+            break;
+    }
 
 }
